8584c5c28c8a441ba2df467e0199df84f7d33acd
bennour007 <hsin.bennour@gmail.com>
2021-01-26 20:03:41 +0100

    week4 unfinshed"

--- week4-2021/prep.R
+++ week4-2021/prep.R
@@ -1,70 +1,83 @@
 library(tidyverse)
 library(janitor)
 library(cowplot)
-library(stringdist)
+library(rKenyaCensus)
 library(sf)
-library(rgeos)
+
+# setting up theme
+ptions(repr.plot.width = 1, repr.plot.height = 0.75)
+
+ma_theme <- theme(
+  line = element_blank(),
+  text = element_text(family = "DejaVu Sans Mono", color = "#4a4a4a"),
+  panel.background = element_rect(fill = "#efe8d1"),
+  panel.grid.major = element_line(colour = "#9ae5de", size = 0.1),
+  panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  axis.text.x = element_text(hjust = 0.7),
+  plot.background = element_rect(fill = "#efe8d1", color = NA),
+  legend.background = element_rect(fill = "#efe8d1"),
+  strip.background = element_blank()
+
+)
+
+# Focusing on education 
 
 
-# Importing and cleaning names 
-gender <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/gender.csv') %>% 
-  clean_names() %>%
-  mutate(county = as_factor(county))
-crops <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/crops.csv') %>% 
-  rename(county = "SubCounty") %>%
-  clean_names() %>%
-  mutate(county = as_factor(county))
-households <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/households.csv') %>% 
-  clean_names() %>%
-  mutate(county = as_factor(county))
+edu <- V4_T2.5 %>% 
+  as_tibble() %>%
+  clean_names() 
 
-###############################################################################
-# unify the county names 
-# gender counties as references
-county_levels <- gender$county %>% levels
 
-levels(crops$county) <- county_levels
-levels(households$county) <- county_levels
+edu_sub_county <- edu %>%
+  filter(sub_county != "KENYA") %>%
+  select(sub_county, gender, total, pre_primary,
+         primary, secondary, university)
 
-################################################################################
-# create one unique dataset 
 
-full <- inner_join(gender, crops, by = "county") %>%
-  inner_join(., households, by = "county")
+counties <- V4_T1.9	%>% pull(County) %>% unique()
 
-################################################################################
-# let the fun begin, Not sure what to start visualizing here!
+edu_county <- edu_sub_county %>% 
+  filter(sub_county %in% counties) %>%
+  pivot_longer(cols = pre_primary:university,
+               names_to = "level",
+               values_to = "reached") 
+  
 
-# let's tidy the data first 
+sf_data <- KenyaCounties_SHP %>% 
+  st_as_sf() %>% 
+  left_join(edu_county, by = c("County" = "sub_county")) 
 
-almost_tidy <- full %>%
-  pivot_longer(cols = male:intersex, 
-               names_to = "gender",
-               values_to = "gender_population") %>%
-  pivot_longer(cols = tea:khat_miraa,
-               names_to = "crop",
-               values_to = "crop_farmers") %>%
-  mutate(farming_ratio = farming/population)
-
-# What about the population in each county
-almost_tidy %>%
-  filter(county != "Total") %>%
-  mutate(county = fct_reorder(county, crop_farmers)) %>%
+map_reached <- sf_data %>%
   ggplot() +
-  geom_point(aes(x = crop_farmers,
-                 y = county,
-                 color = farming_ratio), 
-             alpha = 0.3) +
-  scale_color_viridis_c() +
-  facet_wrap(~ crop)
+  geom_sf(aes(fill = reached), size = 0.1) +
+  scale_fill_viridis_c(direction = -1) +
+  facet_grid(level ~ gender) +
+  ma_theme +
+  labs(title = "Education level reached by the Kenyan population",
+       subtitle = "Divided by ducation level and gender")+
+       #caption = "Bennour's|www.bennour.xyz")+
+  theme(panel.border = element_blank(),
+        strip.background = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text.x = element_blank(),
+        plot.background = element_rect(fill = "#efe8d1", color = NA))
 
-# What are areas with the most farming ratio
-
-full %>%
-  mutate(farming_ratio = farming/population) %>%
-  mutate(county = fct_reorder(county,farming_ratio)) %>%
+cols_reached <-  edu_county %>%
+  mutate(sub_county = fct_reorder(sub_county, reached, sum)) %>%
   ggplot() +
-  geom_col(aes(x = farming_ratio, 
-                 y = county, 
-               fill = )) +
-  scale_x_continuous(position = "top", labels = scales::percent)
+  geom_col(aes(x = reached,
+               y = sub_county,
+               fill = level)) +
+  scale_x_continuous(labels = scales::comma) +
+  scale_fill_viridis_d() +
+  facet_wrap(~ gender) +
+  ma_theme 
+
+library(patchwork)
+
+map_reached + cols_reached 
+ggsave("test.png", device = "png", scale = 2)
